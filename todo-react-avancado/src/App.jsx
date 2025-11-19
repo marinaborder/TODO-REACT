cb54c9d988f65ba5ba48a34ea8c1e6f287efdf74
marinaborder <marinaborder@gmail.com>
2025-11-19 19:24:53 -0300

    finalizando projeto

--- todo-react-avancado/src/App.jsx
+++ todo-react-avancado/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Todo from "./components/Todo"
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 import "./App.css";
 
 
@@ -8,34 +10,77 @@ function App() {
   const [todos, setTodos] = useState([
     {
     id: 1,
-    text: "criar sistema X",
+    text: "Terminar Projeto",
     category: "Trabalho",
     isCompleted: false,
     },
     {
     id: 2,
-    text: "cademia",
+    text: "Fazer Mercado",
     category: "Pessoal",
     isCompleted: false,
     },
-    {
-    id: 3,
-    text: "estudar sistema Z",
-    category: "Estudos",
-    isCompleted: false,
-    },
-  ])
+  ]);
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All")
   
 
-  return <div className="app">
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo todo={todo} />
-      ))}
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos, 
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter(todo => 
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => 
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
+
+  return( 
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <TodoForm addTodo={addTodo} />
+      <h2>Tarefas:</h2>
+      <div className="todo-list">
+        {todos
+
+        .filter((todo) => 
+        filter === "All" 
+        ? true 
+        : filter === "Completed" 
+        ? todo.isCompleted 
+        : !todo.isCompleted)
+
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+
+        .map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
+        ))}
     </div>
-    <TodoForm />
+    <Filter filter={filter} setFilter={setFilter} />
+    <Search search={search} setSearch={setSearch}/>
   </div>
+  );
 }
 
 export default App
--- todo-react-avancado/src/components/TodoForm.jsx
+++ todo-react-avancado/src/components/TodoForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import {useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value || !category) return;
+    addTodo(value, category);
+    setValue("");
+    setCategory("");
+    console.log(value, category);
+  };
+
   return  <div className='todo-form'>
     <h2>Criar Tarefa:</h2>
 
-    <form>
-      <input type="text" placeholder='Digíte a Tarefa'/>
-      <select>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder='Digíte a Tarefa'
+        value={value} 
+        onChange = {(e) => setValue(e.target.value)}
+      />
+      <select value={category} onChange = {(e) => setCategory(e.target.value)}>
         <option value="">Selecione uma categoria</option>
         <option value="Trabalho">Trabalho</option>
         <option value="Estudo">Estudo</option>
